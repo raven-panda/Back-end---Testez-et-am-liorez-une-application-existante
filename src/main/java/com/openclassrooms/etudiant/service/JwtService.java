1ef9f1897d66071eaa642d480bc397a40e3b06cd
raven-panda <ambroise.maire7@gmail.com>
2026-03-27 16:35:15 +0100

    feat(jwt): implemented JWT generation
    Using Jwts builder, user details created in UserService.login method, and the previously added environment variables

--- src/main/java/com/openclassrooms/etudiant/service/JwtService.java
+++ src/main/java/com/openclassrooms/etudiant/service/JwtService.java
@@ -1,14 +1,48 @@
 package com.openclassrooms.etudiant.service;
 
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
 
 @Service
 public class JwtService {
 
+    @Value("${etudiant-backend.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${etudiant-backend.jwtExpirationTime}")
+    private Long jwtExpirationTime;
+
+    /**
+     * Generates a key using Base64 encoded JWT secret (defined in environment).
+     * @return A key ready to use with {@link Jwts} builder to create JWT
+     * @throws io.jsonwebtoken.security.WeakKeyException If the secret is too weak (bit length < 256)
+     * @see Keys#hmacShaKeyFor
+     */
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    /**
+     * Generates a JWT using provided user details (mostly username and password).
+     * Its duration is defined in environment.
+     * @param userDetails User details used for JWT generation
+     * @return Encoded JWT ready to use
+     */
     public String generateToken(UserDetails userDetails) {
-        return null; // TODO
+        return Jwts.builder()
+            .subject((userDetails.getUsername()))
+            .issuedAt(new Date())
+            .expiration(new Date((new Date()).getTime() + jwtExpirationTime))
+            .signWith(key())
+            .compact();
     }
 
 }
