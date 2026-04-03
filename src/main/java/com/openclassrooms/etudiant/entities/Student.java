ac9311e74a466c03b7081fb3399d6f5f90720ddc
raven-panda <ambroise.maire7@gmail.com>
2026-04-03 13:04:07 +0200

    style: reformat code and clean imports

--- src/main/java/com/openclassrooms/etudiant/entities/Student.java
+++ src/main/java/com/openclassrooms/etudiant/entities/Student.java
@@ -1,18 +1,20 @@
 package com.openclassrooms.etudiant.entities;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -26,7 +28,7 @@ public class Student {
     private Long id;
 
     @OneToOne
-    @JoinColumn( name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @CreationTimestamp
